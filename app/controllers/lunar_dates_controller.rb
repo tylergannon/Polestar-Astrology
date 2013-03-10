b12354b228d9625069dc7bfe096fa0c811e4cf79
Tyler Gannon <tgannon@gmail.com>
2013-03-10 16:41:01 -0700

    Lunar dates controller should always return JSON format.

--- app/controllers/lunar_dates_controller.rb
+++ app/controllers/lunar_dates_controller.rb
@@ -4,7 +4,7 @@ class LunarDatesController < ApplicationController
   def show
     date = DateTime.parse(params[:id])
     four_pillars = LunarDate.from_date_time(date)
-    respond_with(four_pillars) do |format|
+    respond_to do |format|
       format.any {
         render text: JSON.generate(four_pillars.as_json) 
       }
